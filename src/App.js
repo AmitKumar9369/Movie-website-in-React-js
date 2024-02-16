bb5660de65d65c5b21fe1ebc5daa5c8fb7ddbf0f
amit <your.sidharth9369@gmail.com>
2024-02-16 01:38:55 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import RouterComponents from "./Route";
+
+function App() {
+  return (
+    <div className="App">
+      <RouterComponents />
+    </div>
+  );
+}
+
+export default App;
